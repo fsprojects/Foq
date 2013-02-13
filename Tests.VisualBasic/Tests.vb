d93ee334af7bcd5bfcc2b8b33db84731c04d4161
ptrelford <ptrelford@localhost>
2013-02-13 22:12:19 +0000

    Adding VB.Net test project

---
+++ Tests.VisualBasic/Tests.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Collections.Generic, NUnit.Framework, Foq.Linq
+
+<TestFixture>
+Public Class Tests
+    <Test>
+    Public Sub TestVerifyFunc()
+        Dim mock =
+            New Mock(Of IList(Of Integer))() _
+                .SetupFunc(Function(x) x.Contains(1)).Returns(True) _
+                .Create()
+        Assert.AreEqual(True, mock.Contains(1))
+    End Sub
+End Class
+
