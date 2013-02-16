c7df9065f4cbed90e1cc0ee3b45b654e778a2167
ptrelford <ptrelford@localhost>
2013-02-16 14:55:13 +0000

    Removed Nuget packages and dependencies from Foq

--- Nuget/content/net40/Foq.Usage.fsx
+++ Nuget/content/net40/Foq.Usage.fsx
@@ -1,5 +1,4 @@
-﻿#r @"..\packages\FSPowerPack.Linq.Community.2.0.0.1\Lib\Net40\FSharp.PowerPack.Linq.dll"
-#r @"..\packages\Foq.0.7\Lib\net40\Foq.dll"
+﻿#r @"..\packages\Foq.0.8\Lib\net40\Foq.dll"
 
 open Foq
 
