bdf878d16473e209ffaf6dac2efc2c8f9c6d766f
ptrelford <ptrelford@localhost>
2013-02-27 09:17:17 +0000

    Updated F# script usage examples to look more like NUnit tests

--- Nuget/content/net45/Foq.Usage.fsx
+++ Nuget/content/net45/Foq.Usage.fsx
@@ -1,6 +1,5 @@
-﻿#r @"..\packages\Foq.0.8\Lib\net45\Foq.dll"
-
-open Foq
+﻿#if INTERACTIVE // Emulate minimal NUnit in F# Interactive
+#r @"..\packages\Foq.0.8\Lib\net45\Foq.dll"
 
 type TestAttribute() = inherit System.Attribute()
 
@@ -12,8 +11,12 @@ module Assert =
     let inline Throws<'T when 'T :> exn> (f) =
         let fail () = failwith "Expected %s" typeof<'T>.Name
         try f (); fail () with :? 'T as e -> e | _ -> fail()
+#else
+open NUnit.Framework
+#endif
+open Foq
 
-let [<Test>] ``setup a method to always return true`` =
+let [<Test>] ``setup a method to always return true`` () =
     // Arrange
     let instance =
         Mock<System.Collections.IList>()
@@ -22,7 +25,7 @@ let [<Test>] ``setup a method to always return true`` =
     // Assert
     Assert.IsTrue(instance.Contains("Anything"))
 
-let [<Test>] ``setup a method to return a value based on the argument`` =
+let [<Test>] ``setup a method to return a value based on the argument`` () =
     // Arrange
     let instance =
         Mock<System.Collections.IList>()
@@ -33,7 +36,7 @@ let [<Test>] ``setup a method to return a value based on the argument`` =
     Assert.AreEqual(true, instance.Contains(1))
     Assert.AreEqual(false,instance.Contains(2))
 
-let [<Test>] ``setup a method to return true or throw predicated on the argument`` =
+let [<Test>] ``setup a method to return true or throw predicated on the argument`` () =
     // Arrange
     let instance =
         Mock<System.Collections.Generic.IList<int>>()
@@ -43,7 +46,7 @@ let [<Test>] ``setup a method to return true or throw predicated on the argument
     // Assert
     Assert.AreEqual(true, instance.Remove(99))
 
-let [<Test>] ``setup a property to always return 1`` =
+let [<Test>] ``setup a property to always return 1`` () =
     // Arrange
     let instance =
         Mock<System.Collections.IList>()
@@ -52,7 +55,7 @@ let [<Test>] ``setup a property to always return 1`` =
     // Assert
     Assert.AreEqual(1, instance.Count)
 
-let [<Test>] ``setup an item property to return float based on the argument`` =
+let [<Test>] ``setup an item property to return float based on the argument`` () =
     // Arrange
     let instance =
         Mock<System.Collections.Generic.IList<double>>()
@@ -63,7 +66,7 @@ let [<Test>] ``setup an item property to return float based on the argument`` =
     Assert.AreEqual(0.0, instance.[0])
     Assert.AreEqual(-1., instance.[1])
 
-let [<Test>] ``setup a method to always raise an exception`` =
+let [<Test>] ``setup a method to always raise an exception`` () =
     // Arrange
     let instance =
         Mock<System.IComparable>()
@@ -74,7 +77,7 @@ let [<Test>] ``setup a method to always raise an exception`` =
     // Assert
     |> Assert.IsTrue
 
-let [<Test>] ``set up an event`` =
+let [<Test>] ``set up an event`` () =
     // Arrange
     let event = Event<_,_>()
     let instance =
@@ -88,7 +91,7 @@ let [<Test>] ``set up an event`` =
     // Assert
     Assert.IsTrue(!triggered)
 
-let [<Test>] ``setup a method to call a function`` =
+let [<Test>] ``setup a method to call a function`` () =
     // Arrange
     let called = ref false
     let instance =
@@ -101,7 +104,7 @@ let [<Test>] ``setup a method to call a function`` =
     // Assert
     Assert.IsTrue(!called)
 
-let [<Test>] ``verify method is called the specified number of times`` =
+let [<Test>] ``verify method is called the specified number of times`` () =
     // Arrange
     let xs = Mock.Of<System.Collections.Generic.IList<int>>()
     // Act
@@ -110,7 +113,7 @@ let [<Test>] ``verify method is called the specified number of times`` =
     Mock.Verify(<@ xs.Contains(0) @>, never)
     Mock.Verify(<@ xs.Contains(any()) @>, once)
 
-let [<Test>] ``expect method is called the specified number of times`` =
+let [<Test>] ``expect method is called the specified number of times`` () =
     // Arrange
     let xs = Mock.Of<System.Collections.Generic.IList<int>>()
     // Assert
@@ -124,7 +127,7 @@ type ICalculator =
     abstract Sum : unit -> unit
     abstract Total : int
 
-let [<Test>] ``mock calculator`` =
+let [<Test>] ``mock calculator`` () =
     // Arrange
     let instance = 
         Mock<ICalculator>()
@@ -136,7 +139,7 @@ let [<Test>] ``mock calculator`` =
     // Assert
     Assert.AreEqual(2, instance.Total)
 
-let [<Test>] ``implement IList interface members`` =
+let [<Test>] ``implement IList interface members`` () =
     // Arrange
     let xs =
         Mock<System.Collections.Generic.IList<char>>.With(fun xs ->
@@ -155,3 +158,18 @@ let [<Test>] ``implement IList interface members`` =
     Assert.Throws<System.ArgumentOutOfRangeException>(fun () ->
         xs.RemoveAt(2)
     ) |> ignore
+
+#if INTERACTIVE // Run tests in F# Interactive
+``setup a method to always return true`` ()
+``setup a method to return a value based on the argument`` ()
+``setup a method to return true or throw predicated on the argument`` ()
+``setup a property to always return 1`` ()
+``setup an item property to return float based on the argument`` ()
+``setup a method to always raise an exception`` ()
+``set up an event`` ()
+``setup a method to call a function`` ()
+``verify method is called the specified number of times`` ()
+``expect method is called the specified number of times`` ()
+``mock calculator`` ()
+``implement IList interface members`` ()
+#endif
