3e71830ee9b49ff8cb31ed58f0beefe31446456f
ptrelford <ptrelford@localhost>
2013-03-19 19:58:33 +0000

    Support for mocking protected members & types containing final/sealed methods

---
+++ Tests/Tests.CSharp/ProtectedTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using Foq.Linq;
+
+public abstract class AbstractBaseClass
+{
+    public abstract int AbstractMethod();
+}
+
+public abstract class ClassWithProtectedMember : AbstractBaseClass
+{
+    abstract protected int ProtectedMember();
+
+    public int Accessor()
+    {
+        return ProtectedMember();
+    }
+
+    public sealed override int AbstractMethod()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+[TestFixture]
+class ProtectedTests
+{
+    [Test]
+    public void TestAccess()
+    {
+        var mock =
+            new Mock<ClassWithProtectedMember>()
+                .SetupByName<int>("ProtectedMember").Returns(1)
+                .Create();
+
+        Assert.AreEqual(1, mock.Accessor());
+    }
+}
+
