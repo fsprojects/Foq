193176396e574ba888ad317698fdb63a8e20a202
ptrelford <ptrelford@localhost>
2013-03-21 19:04:31 +0000

    Support C# ref arguments

--- Tests/Tests.CSharp/ByRefTests.cs
+++ Tests/Tests.CSharp/ByRefTests.cs
@@ -4,18 +4,28 @@ using Foq.Linq;
 public interface IFoo
 {
     bool TryFoo(out int value);
+    int GetTwo(ref int one);
 }
 
 [TestFixture]
 public class ByRefTests
-{
+{    
     [Test]
-    public void ShouldHandleByRefArg()
+    public void ShouldHandleOutArg()
     {
         int x ;
         var foo = new Mock<IFoo>().Setup(mock => mock.TryFoo(out x)).Returns(true).Create();
         bool success = foo.TryFoo(out x);
         Assert.IsTrue(success);
+    }
+
+    [Test]
+    public void ShouldHandleRefArg()
+    {
+        int one = 1;
+        var foo = new Mock<IFoo>().Setup(mock => mock.GetTwo(ref one)).Returns(2).Create();
+        int two = foo.GetTwo(ref one);
+        Assert.AreEqual(2, two);
     }
 }
 
