6bf5dc5155ddf0a6951b6a3b8cf01c6854883ca6
ptrelford <ptrelford@localhost>
2013-03-23 11:21:15 +0000

    Support for setup of out arguments

--- Tests/Tests.CSharp/ByRefTests.cs
+++ Tests/Tests.CSharp/ByRefTests.cs
@@ -4,6 +4,7 @@ using Foq.Linq;
 public interface IFoo
 {
     bool TryFoo(out int value);
+    bool TryFoo2(out int v1, out string v2);
     int GetTwo(ref int one);
 }
 
@@ -13,10 +14,26 @@ public class ByRefTests
     [Test]
     public void ShouldHandleOutArg()
     {
-        int x ;
+        int x = 1;
         var foo = new Mock<IFoo>().Setup(mock => mock.TryFoo(out x)).Returns(true).Create();
+        x = 2;
         bool success = foo.TryFoo(out x);
         Assert.IsTrue(success);
+        Assert.AreEqual(1, x);
+    }
+
+    [Test]
+    public void ShouldHandleTwoOutArgs()
+    {
+        int x = 1;
+        string s = "Hello World";
+        var foo = new Mock<IFoo>().Setup(mock => mock.TryFoo2(out x, out s)).Returns(true).Create();
+        x = -1;
+        s = "Not Hello World";
+        bool success = foo.TryFoo2(out x, out s);
+        Assert.IsTrue(success);
+        Assert.AreEqual(1, x);
+        Assert.AreEqual("Hello World", s);
     }
 
     [Test]
