0fa96bcd1f733aa0fe24a3ea0d79dae934f240f4
Phillip Trelford <phil@trelford.com>
2013-12-02 19:29:03 +0000

    Bits for Foq 1.4 release

--- Foq/AssemblyInfo.fs
+++ Foq/AssemblyInfo.fs
@@ -8,7 +8,7 @@ open System.Runtime.InteropServices
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [<assembly: AssemblyTitle("Foq")>]
-[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyDescription(".Net Mocking Library")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("Foq")>]
@@ -33,8 +33,8 @@ open System.Runtime.InteropServices
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.9.*")]
-[<assembly: AssemblyVersion("0.9.0.0")>]
-[<assembly: AssemblyFileVersion("0.9.0.0")>]
+// [assembly: AssemblyVersion("1.4.*")]
+[<assembly: AssemblyVersion("1.4.0.0")>]
+[<assembly: AssemblyFileVersion("1.4.0.0")>]
 
 ()
