f6643304b9c75db3b60d1285606d33b3476da88e
Phillip Trelford <phil@trelford.com>
2013-12-14 23:28:48 +0000

    Added tea example

---
+++ Examples/TeaExample.fs
@@ -0,0 +1,28 @@
+﻿// Code based on samples in Russel Allen's 
+// Unit Testing Good Patterns #3 - Know Your Moq Argument Matchers
+// http://russellallen.info/post/2011/06/29/Unit-Testing-Good-Patterns-3-Know-Your-Moq-Argument-Matchers!.aspx
+module TeaExample
+
+type CupOfTea () = class end
+
+type ITeaBag =
+    abstract CupOfTea: CupOfTea with get, set
+    abstract Brew: unit -> unit
+
+let Provide(teabag:ITeaBag) =
+  let cupOfTea = CupOfTea()
+  teabag.CupOfTea <- cupOfTea
+  teabag.Brew()
+  cupOfTea
+
+open Foq
+open NUnit.Framework
+
+let [<Test>] Provide_TeaBagIsBrewed() =
+  let teabag = Mock.Of<ITeaBag>()
+  let cuppa = Provide(teabag)
+  verify <@ teabag.Brew() @> once
+
+
+
+
