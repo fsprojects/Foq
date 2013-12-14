530a575cf24a6b11ed9ecd79b6b5293523ba4cd9
Phillip Trelford <phil@trelford.com>
2013-12-14 23:13:06 +0000

    Added greeting example

---
+++ Examples/GreetingExample.fs
@@ -0,0 +1,30 @@
+﻿// Based on code samples in
+// Getting started with JMock1
+// http://www.whiteboxtest.com/Mocking-JMock.php
+module GreetingExample
+
+type GreetingTime =
+    abstract GetGreeting: unit -> string
+
+type Greeting(gt:GreetingTime) =
+    member greeting.SayGreeting(name) =
+        let greeting = gt.GetGreeting()
+        greeting + "," + name
+       
+open Foq
+open NUnit.Framework
+
+[<Test>]
+let ``expect greeting`` () =
+    // Arrange
+    let goodNight = 
+        Mock<GreetingTime>
+            .Method(fun gt -> <@ gt.GetGreeting @>)
+            .Returns("Good Night")
+    expect <@ goodNight.GetGreeting() @> once
+    let greet = Greeting(goodNight)
+    // Act
+    let out = greet.SayGreeting("Mr. Sam")
+    // Assert
+    Assert.AreEqual("Good Night,Mr. Sam", out)
+    verifyAll goodNight
