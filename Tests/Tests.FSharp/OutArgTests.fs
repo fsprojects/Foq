83bb9da54f3d7f10cd4539c3c8dc05445aeeb6b0
Phillip Trelford <phil@trelford.com>
2014-02-06 06:57:54 +0000

    Unit tests for mocking C# out arguments from F#

---
+++ Tests/Tests.FSharp/OutArgTests.fs
@@ -0,0 +1,33 @@
+﻿module OutArgTests
+
+open NUnit.Framework
+open Foq
+
+let [<Test>] ``one out arg no result`` () =
+    let mock = Mock<IHaveOutArgs>().SetupByName("NoInOneOutNoResult").Returns(1).Create()
+    Assert.AreEqual(1, mock.NoInOneOutNoResult())
+
+let [<Test>] ``one out arg result`` () =
+    let mock = Mock<IHaveOutArgs>().SetupByName("NoInOneOutResult").Returns((true,2)).Create()
+    let success, n = mock.NoInOneOutResult()
+    Assert.IsTrue(success)
+    Assert.AreEqual(2, n)
+
+let [<Test>] ``one in one out arg result`` () =
+    let mock = Mock<IHaveOutArgs>().SetupByName("OneInOneOutResult").Returns((true,3)).Create()
+    let success, n = mock.OneInOneOutResult(1)
+    Assert.IsTrue(success)
+    Assert.AreEqual(3, n)
+
+let [<Test>] ``two out args no result`` () =
+    let mock = Mock<IHaveOutArgs>().SetupByName("NoInTwoOutNoResult").Returns((1,2)).Create()
+    let x, y = mock.NoInTwoOutNoResult()
+    Assert.AreEqual(1, x)
+    Assert.AreEqual(2, y)
+
+let [<Test>] ``three out args result`` () =
+    let mock = Mock<IHaveOutArgs>().SetupByName("NoInThreeOutResult").Returns((true,1,2,3)).Create()
+    let success, x, y, z = mock.NoInThreeOutResult()
+    Assert.AreEqual(1, x)
+    Assert.AreEqual(2, y)
+    Assert.AreEqual(3, z)
