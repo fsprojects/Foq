0cb94e3eeb4bd2a65d8a348981c163466a6d3ae4
Phillip Trelford <phil@trelford.com>
2014-02-06 06:57:54 +0000

    Unit tests for mocking C# out arguments from F#

---
+++ Tests/Types.CSharp/IHaveOutArgs.cs
@@ -0,0 +1,9 @@
+﻿public interface IHaveOutArgs
+{
+    void NoInOneOutNoResult(out int x);
+    bool NoInOneOutResult(out int x);
+    bool OneInOneOutResult(int x, out int y);
+    void NoInTwoOutNoResult(out int x, out int y);
+    bool NoInThreeOutResult(out int x, out int y, out int z);
+}
+
