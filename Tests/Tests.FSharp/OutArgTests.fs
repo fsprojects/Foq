b611ae0c5177130742fced6850c568bad36743ae
Phillip Trelford <phil@trelford.com>
2014-02-06 07:41:23 +0000

    Out argument support with returns via function

--- Tests/Tests.FSharp/OutArgTests.fs
+++ Tests/Tests.FSharp/OutArgTests.fs
@@ -26,6 +26,35 @@ let [<Test>] ``two out args no result`` () =
     Assert.AreEqual(2, y)
 
 let [<Test>] ``three out args result`` () =
+    let mock = Mock<IHaveOutArgs>().SetupByName("NoInThreeOutResult").ReturnsFunc(fun () -> (true,1,2,3)).Create()
+    let success, x, y, z = mock.NoInThreeOutResult()
+    Assert.AreEqual(1, x)
+    Assert.AreEqual(2, y)
+    Assert.AreEqual(3, z)
+
+let [<Test>] ``one out arg no result via function`` () =
+    let mock = Mock<IHaveOutArgs>().SetupByName("NoInOneOutNoResult").ReturnsFunc(fun () -> 1).Create()
+    Assert.AreEqual(1, mock.NoInOneOutNoResult())
+
+let [<Test>] ``one out arg result  via function`` () =
+    let mock = Mock<IHaveOutArgs>().SetupByName("NoInOneOutResult").ReturnsFunc(fun () -> (true,2)).Create()
+    let success, n = mock.NoInOneOutResult()
+    Assert.IsTrue(success)
+    Assert.AreEqual(2, n)
+
+let [<Test>] ``one in one out arg result  via function`` () =
+    let mock = Mock<IHaveOutArgs>().SetupByName("OneInOneOutResult").ReturnsFunc(fun () -> (true,3)).Create()
+    let success, n = mock.OneInOneOutResult(1)
+    Assert.IsTrue(success)
+    Assert.AreEqual(3, n)
+
+let [<Test>] ``two out args no result  via function`` () =
+    let mock = Mock<IHaveOutArgs>().SetupByName("NoInTwoOutNoResult").Returns((1,2)).Create()
+    let x, y = mock.NoInTwoOutNoResult()
+    Assert.AreEqual(1, x)
+    Assert.AreEqual(2, y)
+
+let [<Test>] ``three out args result via function`` () =
     let mock = Mock<IHaveOutArgs>().SetupByName("NoInThreeOutResult").Returns((true,1,2,3)).Create()
     let success, x, y, z = mock.NoInThreeOutResult()
     Assert.AreEqual(1, x)
